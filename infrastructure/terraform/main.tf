40255540c15fad7a3d3b6be25ead2b7bc20a4542
Pedro Roque de Mattia <pedro.mattia@invistafidc.com.br>
2025-10-13 19:54:24 -0300

    feat(infrastructure): manage Terraform-provisioned servers using Ansible

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -7,17 +7,55 @@ resource "digitalocean_droplet" "server" {
   name   = "main"
   image  = "ubuntu-24-04-x64"
   size   = "s-2vcpu-4gb"
-  region = "SFO3" # san francisco
+  region = "SFO3" # San Francisco
 
   ssh_keys = [digitalocean_ssh_key.repository.fingerprint]
 }
 
-resource "ansible_group" "server" {
-  name = "server"
+# Sets up a firewall directly on DigitalOcean. This prevents that misconfigured firewall rules on 
+# the server itself would allow unwanted access to it, such as when using docker with ufw.
+resource "digitalocean_firewall" "main" {
+  name        = "main"
+  droplet_ids = [digitalocean_droplet.server.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22" # SSH
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53" # DNS
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53" # DNS
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "80" # HTTP
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "443" # HTTPS
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
 }
 
-resource "ansible_host" "server" {
-  name   = "server"
+resource "ansible_group" "server" {
+  name = "servers"
+}
+
+resource "ansible_host" "main" {
+  name   = "main"
   groups = [ansible_group.server.name]
   variables = {
     ansible_host = digitalocean_droplet.server.ipv4_address
